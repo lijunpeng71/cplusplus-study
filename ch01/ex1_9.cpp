55f06c85ccfdd303ca6fcbd71cba7b36c744872d
lijunpeng71 <lijunpeng71@163.com>
2020-01-10 11:46:33 +0800

    modify

--- ch01/ex1_9.cpp
+++ ch01/ex1_9.cpp
@@ -1,4 +1,18 @@
+#include <iostream>
+using namespace std;
+
+auto sum(int lo, int hi)
+{
+	int sum = 0;
+	while (lo <= hi)
+	{
+		sum += lo++;
+	}
+	return sum;
+}
+
 int main()
 {
-    return 0;
+	cout << "Sum of 50 to 100 inclusive is:" << sum(50, 100) << endl;
+	return 0;
 }
